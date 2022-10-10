54f947a3589bd1121dafbd294c336aecdc77a479
Абя Булхуков <Minsk5545@gmail.com>
2022-10-10 17:24:18 +0300

    vvvvvv

--- homework.py
+++ homework.py
@@ -1,4 +1,4 @@
-class InfoMessage:
+ class InfoMessage:
     """Информационное сообщение о тренировке."""
     def __init__(self,
                  training_type: str,
@@ -17,7 +17,7 @@ class InfoMessage:
         message = (f'Тип тренировки: {self.training_type}; '
                    f'Длительность: {self.duration:.3f} ч.; '
                    f'Дистанция: {self.distance:.3f} км; '
-                   f'Средняя. скорость: {self.speed:.3f} км/ч; '
+                   f'Ср. скорость: {self.speed:.3f} км/ч; '
                    f'Потрачено ккал: {self.calories:.3f}.')
         return message
 
@@ -29,12 +29,12 @@ class Training:
     """Базовый класс тренировки."""
     LEN_STEP = 0.65
     M_IN_KM = 1000
+
     def __init__(self,
                  action: int,
                  duration: float,
                  weight: float,
                  ) -> None:
-
         self.action = action
         self.duration = duration
         self.weight = weight
@@ -59,9 +59,10 @@ class Training:
 
 
 class Running(Training):
-    """Тренировка"""
+    """Тренировка: бег."""
     coeff_calorie_1 = 18
     coeff_calorie_2 = 20
+
     def get_spent_calories(self):
         first = self.coeff_calorie_1 * self.get_mean_speed()
         second = self.duration * 60
@@ -82,6 +83,7 @@ class SportsWalking(Training):
                  ) -> None:
         super().__init__(action, duration, weight)
         self.height = height
+
     def get_spent_calories(self):
         first_step = self.coeff_calorie_1 * self.weight
         second_step = (self.get_mean_speed()**2 // self.height)
@@ -91,15 +93,11 @@ class SportsWalking(Training):
 
 
 class Swimming(Training):
-    """ Тренировка: плавание.
-    length_pool — длина бассейна в метрах
-    count_pool — сколько раз пользователь переплыл бассейн
-    LEN_STEP - один гребок
-    get_spent_calories() - расчета калорий
-    get_mean_speed() - средняя скорость"""
+    """Тренировка: плавание."""
     LEN_STEP = 1.38
     coeff_calorie_1 = 1.1
     coeff_calorie_2 = 2
+
     def __init__(self,
                  action: int,
                  duration: float,
@@ -107,7 +105,6 @@ class Swimming(Training):
                  length_pool: float,
                  count_pool: float
                  ) -> None:
-
         super().__init__(action, duration, weight)
         self.length_pool = length_pool
         self.count_pool = count_pool
@@ -122,15 +119,10 @@ class Swimming(Training):
 
 
 def read_package(workout_type: str, data: list) -> Training:
-    """Прогнать данные полученные от датчиков.
-    на вход параметры:
-       Словарь из двух параметров
-        1) Строка код тренировки
-        2) Класс обработчик тренировки"""
+    """Прочитать данные полученные от датчиков."""
     prime = {'SWM': Swimming, 'RUN': Running, 'WLK': SportsWalking}
     if workout_type in prime:
-        return prime[workout_type](*data) 
-
+        return prime[workout_type](*data) #???????????????????????????????
 
 def main(training: Training) -> None:
     """Главная функция."""
